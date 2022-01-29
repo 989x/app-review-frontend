40ce9a3189188e6fcc73842326a35e80e6475b49
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-01-29 13:13:55 +0700

    login & register 👨🏻‍🎨

--- client/components/Outline/Navbar.jsx
+++ client/components/Outline/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="bg-white border-gray-200 px-2 sm:px-6 pt-4 pb-2 rounded dark:bg-white sticky top-0 z-50">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
 
-        <a href="#" className="flex">
+        <a href="/" className="flex">
           <div className="pr-4">
             <img 
               src="/img/exploding-head.png" 
@@ -42,18 +42,10 @@ const Navbar = () => {
 
         <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-2" id="mobile-menu-3">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+           
             <li>
               <a 
-                href="#" 
-                className="text-base block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" 
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a 
-                href="#" 
+                href="SubmitReview" 
                 className="text-base block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Wrtie review
@@ -61,7 +53,7 @@ const Navbar = () => {
             </li>
             <li>
               <a 
-                href="#" 
+                href="LoginScreen" 
                 className="text-base block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Login
